85b14bf426329fe5ae7677c6751eda3b9dbbcf04
Haytham <haytham.saqer31@gmail.com>
2021-01-18 23:57:03 +0200

    Finished chapter one

--- pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include  # new
-from .views import homePageView
-from . import views
-urlpatterns = [
-
-    path('', views.homePageView),  # new
-]
--- test_project/urls.py
+++ test_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include  # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),  # new
+    path('blog/', include('blog.urls', namespace='blog')),  # new
 ]
